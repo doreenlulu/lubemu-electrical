ddb562a47851fa509d67aadfa8f3b72a4bf76a5f
Doreen <doreenlulu97@gmail.com>
2023-08-31 12:45:19 +0300

    nav bar done

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -2,16 +2,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '/home/doreen/lubemu-electrical/src/assets/logo.jpg'
+import './Nav.css'
 
 function Nav() {
   return (
     <>
     <nav className='navbar' >
       <div className='navbar-container'>
-      <Link to='/' className='navbar-logo'>
-        <img src={logo} />
-         LSS <i className='fab fa-codepen' />
-      </Link>
+      <Link to='/'><img src={logo} className='navbar-logo'/>Lubemu Electrical Company </Link>
+      <Link to ='/' className='nav-links' >Home</Link>
+      <Link to ='/about' className='nav-links' >About</Link>
+      <Link to ='/services' className='nav-links' >Services</Link>
+      <Link to ='/faq' className='nav-links' >FAQ</Link>
+      <Link to ='/blog' className='nav-links' >Blog</Link>
       </div>
     </nav>
     </>
